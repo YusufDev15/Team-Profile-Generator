572b17896d044b463f5f03984717545918f616cf
Yusuf <145934201+YusufDev15@users.noreply.github.com>
2024-01-02 19:29:19 +0000

    complete team members added to index.js with a file path to create an output folder with a HTML file

--- starter/index.js
+++ starter/index.js
@@ -110,3 +110,164 @@ const promptMenu = () => {
       }
     });
 };
+
+const promptEngineer = () => {
+  console.log(`
+    ------------------
+    Add a New Engineer
+    ------------------
+    `);
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of the engineer? (Required)",
+        validate: (engineerName) => {
+          if (engineerName) {
+            return true;
+          } else {
+            console.log("Please enter the name of the engineer!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "employedId",
+        message: "Enter your employee ID (Required)",
+        validate: (employeeId) => {
+          if (employeeId) {
+            return true;
+          } else {
+            console.log("Please enter your employee ID!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter your email address (Required)",
+        validate: (email) => {
+          if (email) {
+            return true;
+          } else {
+            console.log("Please enter your email address!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "githubUsername",
+        message: "Enter your Github username (Required)",
+        validate: (githubUsername) => {
+          if (githubUsername) {
+            return true;
+          } else {
+            console.log("Please enter your Github username!");
+            return false;
+          }
+        },
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+      const engineer = new Engineer(
+        answers.name,
+        answers.employeeId,
+        answers.email,
+        answers.githubUsername
+      );
+      teamMembers.push(engineer);
+      promptMenu();
+    });
+};
+
+const promptIntern = () => {
+  console.log(`
+      ------------------
+      Add a New Intern
+      ------------------
+      `);
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of the intern? (Required)",
+        validate: (internName) => {
+          if (internName) {
+            return true;
+          } else {
+            console.log("Please enter the name of the intern!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "employedId",
+        message: "Enter your employee ID (Required)",
+        validate: (employeeId) => {
+          if (employeeId) {
+            return true;
+          } else {
+            console.log("Please enter your employee ID!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter your email address (Required)",
+        validate: (email) => {
+          if (email) {
+            return true;
+          } else {
+            console.log("Please enter your email address!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Enter your school name (Required)",
+        validate: (school) => {
+          if (school) {
+            return true;
+          } else {
+            console.log("Please enter your school name!");
+            return false;
+          }
+        },
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+      const intern = new Intern(
+        answers.name,
+        answers.employeeId,
+        answers.email,
+        answers.school
+      );
+      teamMembers.push(intern);
+      promptMenu();
+    });
+};
+
+const buildTeam = () => {
+  console.log(`
+    ------------------
+    Finished building my team!
+    ------------------
+    `);
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+  fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+};
+promptManager;
